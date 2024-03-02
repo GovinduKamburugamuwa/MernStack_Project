373f291dd395c9fe761fe4f4090e4da5abf83d66
GovinduKamburugamuwa <114232870+GovinduKamburugamuwa@users.noreply.github.com>
2024-03-02 13:57:36 +0530

    test post request using postman and mongodb

--- routes/workoutes.js
+++ routes/workoutes.js
@@ -1,34 +1,37 @@
 const express = require('express')
+const Workout = require('../models/workoutModels')
 
 const router = express.Router()
 //GET request
-router.get('/',(req,res)=>
-{
-    res.json({mssg:'GET request'})
+router.get('/', (req, res) => {
+    res.json({ mssg: 'GET request' })
 })
 
 //GET single request
-router.get('/:id',(req,res)=>
-{
-    res.json({mssg:'GET a single request'})
+router.get('/:id', (req, res) => {
+    res.json({ mssg: 'GET a single request' })
 })
 
 // POST a request
-router.post('/',(req,res)=>
-{
-    res.json({mssg:'POST request'})
+router.post('/', async (req, res) => {
+    const { title, rep, loads } = req.body
+    try {
+        const workout = await Workout.create({ title, rep, loads })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
 })
 
 //DELETE a single request
-router.delete('/:id',(req,res)=>
-{
-    res.json({mssg:'DELETE a single request'})
+router.delete('/:id', (req, res) => {
+    res.json({ mssg: 'DELETE a single request' })
 })
 
 //UPDATE a single request
-router.patch('/:id',(req,res)=>
-{
-    res.json({mssg:'Update a single request'})
+router.patch('/:id', (req, res) => {
+    res.json({ mssg: 'Update a single request' })
 })
 
 module.exports = router
