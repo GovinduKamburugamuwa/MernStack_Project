d41604f566ec7ba6f5d9aaa94fcc90cb068b5248
GovinduKamburugamuwa <114232870+GovinduKamburugamuwa@users.noreply.github.com>
2024-03-22 23:10:55 +0530

    make WorkoutForm File

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+    const [title,SetTitle] = useState('')
+    const [loads,SetLoads] = useState('')
+    const [rep,SetRep] = useState('')
+    
+    return (
+
+
+
+      );
+}
+ 
+export default WorkoutForm;
